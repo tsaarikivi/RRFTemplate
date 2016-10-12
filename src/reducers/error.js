898be473ae977706f68ba7b655b60d8e5d0511da
Tero Saarikivi <tero.saarikivi@outlook.com>
2016-10-12 11:06:29 +0300

    errors

--- src/reducers/error.js
+++ src/reducers/error.js
@@ -4,8 +4,18 @@ const INITIAL_STATE = null
 
 export default (state = INITIAL_STATE, args) => {
     switch (args.type) {
-        case authTypes.REGISTER+'_ERROR':
+        case authTypes.LOGIN + '_ERROR':
+            return 'Login error'
+        case authTypes.LOGOUT + '_ERROR':
+            return 'Logout error'
+        case authTypes.REGISTER + '_ERROR':
             return 'Register error'
+        case authTypes.ADD_USER + '_ERROR':
+            return 'Add user error'
+        case authTypes.FETCH_USERS + '_ERROR':
+            return 'Fetch users error'
+        case authTypes.DELETE_USER + '_ERROR':
+            return 'Delete user error'
         default:
             return state
     }
